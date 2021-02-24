1f49f3eebd1cdf95d84edd0c6dd97e407f6df2ee
Eben Swart <ebenswart12@gmail.com>
2021-02-24 10:45:14 +0200

    Fixed user greeting error

--- prg381.java
+++ prg381.java
@@ -6,7 +6,7 @@ public class prg381 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);        
         String name = input.nextLine();
-        System.out.println("Hello %s, it is nice to meet you!");
+        System.out.printf("Hello %s, it is nice to meet you!", name);
         input.close();
     }
 }
