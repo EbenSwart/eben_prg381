8b3f1664d73775dea537f0799c51d0d0e51c0be1
Eben Swart <ebenswart12@gmail.com>
2021-02-24 10:44:09 +0200

    Add user greeting

--- prg381.java
+++ prg381.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 public class prg381 {
 
     public static void main(String[] args) {
-        System.out.println("First Java file on Git");
+        Scanner input = new Scanner(System.in);        
+        String name = input.nextLine();
+        System.out.println("Hello %s, it is nice to meet you!");
+        input.close();
     }
 }
